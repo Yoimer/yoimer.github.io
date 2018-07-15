bf5b7d8c94709ed8a0fd002cf971766b80bb76e7
Yoimer <ydrf19@gmail.com>
2018-07-15 17:53:47 -0400

    displaying the current month and year

--- app2.js
+++ app2.js
@@ -162,7 +162,8 @@ var UIController = (function() {
         expensesLabel:'.budget__expenses--value',
         percentageLabel:'.budget__expenses--percentage',
         container:'.container',
-        expensesPercLabel:'.item__percentage'
+        expensesPercLabel:'.item__percentage',
+        dataLabel:'.budget__title--month'
     };
 
     var formatNumber = function(num, type){
@@ -284,6 +285,19 @@ var UIController = (function() {
             });
         },
 
+        displayMonth: function(){
+            var now, month, months, year;
+            
+            var now = new Date();
+            //var christmas = new Date(2016, 11, 25);
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Semptember', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dataLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -387,6 +401,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
